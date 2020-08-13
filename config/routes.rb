b830fc6571b9dad00d909b42b1edfe77747756b6
Robert Smith <rsmithlal@gmail.com>
2020-08-12 21:26:56 -0400

    added sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  mount Sidekiq::Web => '/sidekiq'
 end
