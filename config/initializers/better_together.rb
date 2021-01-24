a2db4c8047a24a72295360bb64942481d8edce24
Robert Smith <rsmithlal@gmail.com>
2021-01-23 23:47:32 -0330

    set default new user password url

--- config/initializers/better_together.rb
+++ config/initializers/better_together.rb
@@ -1,4 +1,11 @@
 require 'better_together'
 
 BetterTogether.user_class = "BetterTogether::User"
-BetterTogether.default_user_confirm_success_url = 'http://localhost:8080'
+BetterTogether.default_user_confirm_success_url = ENV.fetch(
+  'APP_HOST',
+  'http://localhost:3000'
+)
+BetterTogether.default_user_new_password_url = ENV.fetch(
+  'APP_HOST',
+  'http://localhost:3000'
+) + '/bt/api/auth/password/new'
