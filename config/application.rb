7b5982b06f79f1c5e624f2ad003e65f7e3e5755c
Rob <rob@bettertogethersolutions.com>
2023-06-08 20:55:01 -0230

    Update rails 7 application files

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,30 +11,12 @@ module BetterTogether
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.action_mailer.smtp_settings = {
-      address: 'smtp.sendgrid.net',
-      port: '587',
-      authentication: :plain,
-      user_name: ENV.fetch('SENDGRID_USERNAME', ''),
-      password: ENV.fetch('SENDGRID_PASSWORD', ''),
-      domain: ENV.fetch(
-        'SMTP_DOMAIN',
-        ENV.fetch('APP_HOST', 'http://localhost:3000')
-      ),
-      enable_starttls_auto: true
-    }
-
-    Rails.application.routes.default_url_options = {
-      host: ENV.fetch('APP_HOST', 'http://localhost:3000')
-    }
-
-    config.action_mailer.default_url_options = {
-      host: ENV.fetch('APP_HOST', 'http://localhost:3000')
-    }
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_012024) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_225837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +19,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.string "authorable_type", null: false
     t.bigint "authorable_id", null: false
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["authorable_type", "authorable_id"], name: "by_authorable", unique: true
     t.index ["bt_id"], name: "authorable_by_bt_id", unique: true
   end
@@ -31,8 +30,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.string "author_type", null: false
     t.bigint "author_id", null: false
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "by_author", unique: true
     t.index ["bt_id"], name: "author_by_bt_id", unique: true
   end
@@ -43,8 +42,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.bigint "author_id", null: false
     t.integer "sort_order"
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["author_id"], name: "by_authorship_author"
     t.index ["authorable_id"], name: "by_authorship_authorable"
     t.index ["bt_id"], name: "authorship_by_bt_id", unique: true
@@ -56,8 +55,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.bigint "creator_id", null: false
     t.string "community_privacy", default: "public", null: false
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["bt_id"], name: "group_by_bt_id", unique: true
     t.index ["community_privacy"], name: "by_group_privacy"
     t.index ["creator_id"], name: "by_creator"
@@ -69,8 +68,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.bigint "community_id", null: false
     t.bigint "role_id", null: false
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["bt_id"], name: "community_membership_by_bt_id", unique: true
     t.index ["community_id", "member_id", "role_id"], name: "unique_community_membership_member_role", unique: true
     t.index ["community_id"], name: "community_membership_by_community"
@@ -86,8 +85,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.bigint "agent_id", null: false
     t.string "bt_id", limit: 100, null: false
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["active", "agent_type", "agent_id"], name: "active_identification", unique: true
     t.index ["active"], name: "by_active_state"
     t.index ["agent_type", "agent_id"], name: "by_agent"
@@ -99,8 +98,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
   create_table "better_together_invitations", force: :cascade do |t|
     t.string "bt_id", limit: 100, null: false
     t.string "status", limit: 20, null: false
-    t.datetime "valid_from", null: false
-    t.datetime "valid_until"
+    t.datetime "valid_from", precision: nil, null: false
+    t.datetime "valid_until", precision: nil
     t.string "invitable_type", null: false
     t.bigint "invitable_id", null: false
     t.string "inviter_type", null: false
@@ -109,8 +108,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.bigint "invitee_id", null: false
     t.bigint "role_id"
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["bt_id"], name: "invitation_by_bt_id", unique: true
     t.index ["invitable_type", "invitable_id"], name: "by_invitable"
     t.index ["invitee_type", "invitee_id"], name: "by_invitee"
@@ -124,9 +123,9 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
   create_table "better_together_jwt_denylists", force: :cascade do |t|
     t.string "bt_id", limit: 100, null: false
     t.string "jti"
-    t.datetime "exp"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "exp", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["bt_id"], name: "jwt_denylist_by_bt_id", unique: true
     t.index ["jti"], name: "index_better_together_jwt_denylists_on_jti"
   end
@@ -136,19 +135,19 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.string "name", limit: 50, null: false
     t.string "description"
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["bt_id"], name: "person_by_bt_id", unique: true
     t.index ["name"], name: "by_name"
   end
 
   create_table "better_together_posts", force: :cascade do |t|
     t.string "bt_id"
-    t.datetime "published_at"
+    t.datetime "published_at", precision: nil
     t.string "post_privacy", default: "public", null: false
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["post_privacy"], name: "by_post_privacy"
     t.index ["published_at"], name: "by_post_publication_date"
   end
@@ -159,8 +158,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.integer "sort_order"
     t.string "target_class", limit: 100
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["bt_id"], name: "role_by_bt_id", unique: true
     t.index ["reserved"], name: "by_reserved_state"
     t.index ["sort_order"], name: "by_sort_order"
@@ -172,14 +171,14 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["bt_id"], name: "user_by_bt_id", unique: true
     t.index ["confirmation_token"], name: "index_better_together_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_better_together_users_on_email", unique: true
@@ -192,8 +191,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.string "sluggable_type", limit: 50
     t.string "scope"
     t.integer "lock_version", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "locale"
     t.index ["locale"], name: "index_friendly_id_slugs_on_locale"
     t.index ["slug", "sluggable_type", "locale"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_locale"
@@ -207,8 +206,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.string "value"
     t.string "translatable_type"
     t.bigint "translatable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["translatable_id", "translatable_type", "key"], name: "index_mobility_string_translations_on_translatable_attribute"
     t.index ["translatable_id", "translatable_type", "locale", "key"], name: "index_mobility_string_translations_on_keys", unique: true
     t.index ["translatable_type", "key", "value", "locale"], name: "index_mobility_string_translations_on_query_keys"
@@ -220,8 +219,8 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.text "value"
     t.string "translatable_type"
     t.bigint "translatable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["translatable_id", "translatable_type", "key"], name: "index_mobility_text_translations_on_translatable_attribute"
     t.index ["translatable_id", "translatable_type", "locale", "key"], name: "index_mobility_text_translations_on_keys", unique: true
   end
@@ -230,18 +229,18 @@ ActiveRecord::Schema.define(version: 2021_01_19_012024) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
