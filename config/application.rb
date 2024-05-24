3c39df401b83d4dc8d3edea81a7e9960df1bde1e
Robert Smith <rob@bettertogethersolutions.com>
2024-05-23 22:38:59 -0230

    Add bettter app config including mailer configs

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 module BetterTogether
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
     config.api_only = false
     config.session_store :cookie_store, key: '_bt_session'
     config.middleware.use ActionDispatch::Cookies
@@ -26,5 +25,43 @@ module BetterTogether
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    I18n.enforce_available_locales = true
+
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    config.active_storage.analyzers = [
+      ActiveStorage::Analyzer::ImageAnalyzer::Vips,
+      ActiveStorage::Analyzer::ImageAnalyzer::ImageMagick,
+      ActiveStorage::Analyzer::VideoAnalyzer,
+      ActiveStorage::Analyzer::AudioAnalyzer
+    ]
+
+
+    config.action_mailer.default_options = {
+      from: ENV.fetch('FROM_ADDRESS', 'Better Together Community <community@bettertogethersolutions.com>'),
+      reply_to: ENV.fetch('REPLY_ADDRESS', 'Better Together Community Support <support@bettertogethersolutions.com>')
+    }
+
+    config.action_mailer.smtp_settings = {
+      :address        => ENV.fetch('SMTP_ADDRESS') { 'smtp.sendgrid.net' },
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV.fetch('SMTP_USERNAME') { 'apikey' },
+      :password       => ENV['SMTP_PASSWORD'],
+      :domain         => ENV.fetch('APP_HOST') { 'localhost:3000'},
+      :enable_starttls_auto => true
+    }
+
+    Rails.application.routes.default_url_options = {
+      host: ENV.fetch('APP_HOST', 'http://localhost:3000')
+    }
+
+    config.action_mailer.default_url_options = {
+      host: ENV.fetch('APP_HOST', 'http://localhost:3000')
+    }
+
+    config.time_zone = 'Newfoundland'
   end
 end
