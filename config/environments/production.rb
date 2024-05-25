bae93ab4216facfe424927420e810a3ceac27b5f
Robert Smith <rob@bettertogethersolutions.com>
2024-05-25 15:48:21 -0230

    Allow setting log level from env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info').to_sym
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
