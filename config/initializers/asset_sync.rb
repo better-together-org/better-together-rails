d720c59e8131b1236d09e54529e7c17dac9db61b
Robert Smith <rob@bettertogethersolutions.com>
2024-06-08 15:37:55 -0230

    Fix Rubocop violations

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,17 +1,17 @@
+# frozen_string_literal: true
+
 if defined?(AssetSync)
   AssetSync.configure do |config|
     config.fog_provider = 'AWS'
-    
+
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-    
-    if ENV.key?('AWS_SESSION_TOKEN')
-      config.aws_session_token = ENV['AWS_SESSION_TOKEN']
-    end
-    
+
+    config.aws_session_token = ENV['AWS_SESSION_TOKEN'] if ENV.key?('AWS_SESSION_TOKEN')
+
     # Ensure that aws_iam_roles is set to false if not explicitly required
     config.aws_iam_roles = ENV['AWS_IAM_ROLES'] == 'true'
-    
+
     config.fog_directory = ENV['FOG_DIRECTORY']
     config.fog_region = ENV['FOG_REGION']
 
@@ -21,7 +21,7 @@ if defined?(AssetSync)
     # config.aws_acl = nil
     # config.fog_host = "s3.amazonaws.com"
     # config.fog_port = "9000"
-    config.fog_scheme = "https"
+    config.fog_scheme = 'https'
     # config.cdn_distribution_id = ENV['CDN_DISTRIBUTION_ID'] if ENV.key?('CDN_DISTRIBUTION_ID')
     # config.invalidate = ['file1.js']
     # config.existing_remote_files = "keep"
