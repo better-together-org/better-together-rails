8355424fdc5974d61b145957f8db94700f28be95
Robert Smith <rob@bettertogethersolutions.com>
2024-09-13 11:22:10 -0230

    Wrap root_path route in locale scope

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@
 Rails.application.routes.draw do
   get 'healthcheck', to: 'healthcheck#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'better_together/pages#show', defaults: { path: 'home-page' }, as: :home_page
-
+  
   scope ':locale', # rubocop:todo Metrics/BlockLength
         locale: /#{I18n.available_locales.join('|')}/,
         defaults: { locale: I18n.locale } do
     resources :partners
+    root to: 'better_together/pages#show', defaults: { path: 'home-page' }, as: :home_page
   end
 
   mount BetterTogether::Engine => '/'
