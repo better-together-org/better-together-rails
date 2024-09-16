a373b70923d0aa9eaf589273311548009ff68bc1
Robert Smith <rob@bettertogethersolutions.com>
2024-09-16 01:34:24 -0230

    Include JourneyStage and Topic concerns in BetterTogether::Page

--- config/initializers/better_together.rb
+++ config/initializers/better_together.rb
@@ -18,7 +18,10 @@ end
 Rails.application.config.to_prepare do
   require_dependency 'better_together/content/block'
   require_dependency 'better_together/navigation_item'
+  require_dependency 'better_together/page'
 
   BetterTogether::Content::Block.include(NewToNlContentBlock)
   BetterTogether::NavigationItem.include(NewToNlNavigationItem)
+  BetterTogether::Page.include(NewToNlJourneyStage)
+  BetterTogether::Page.include(NewToNlTopic)
 end
