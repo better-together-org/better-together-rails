cc79ab26baa736b75a9591adb6f222aac898825d
Robert Smith <rob@bettertogethersolutions.com>
2024-09-23 23:58:15 -0230

    Ensure that no default locale is set for routes
    update better_together

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@
 Rails.application.routes.draw do
   get 'healthcheck', to: 'healthcheck#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   scope ':locale', # rubocop:todo Metrics/BlockLength
-        locale: /#{I18n.available_locales.join('|')}/,
-        defaults: { locale: I18n.locale } do
-    root to: 'better_together/pages#show', defaults: { path: 'home-page' }, as: :home_page
+        locale: /#{I18n.available_locales.join('|')}/ do
+
+    end
   end
 
   root to: redirect("/#{I18n.default_locale}")
