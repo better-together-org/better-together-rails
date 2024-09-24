a9d041c90b11317b3bd1a19a029d8aff393fbdfc
Robert Smith <rob@bettertogethersolutions.com>
2024-09-24 21:20:32 -0230

    Add page_url to page view table

---
+++ db/migrate/20240924224112_add_page_url_better_together_metrics_page_view.better_together.rb
@@ -0,0 +1,6 @@
+# This migration comes from better_together (originally 20240924224024)
+class AddPageUrlBetterTogetherMetricsPageView < ActiveRecord::Migration[7.1]
+  def change
+    add_column :better_together_metrics_page_views, :page_url, :string
+  end
+end
