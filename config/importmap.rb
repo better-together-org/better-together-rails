56ea8213c1be82fc3a1329f108615489fc18ad5f
Robert Smith <rob@bettertogethersolutions.com>
2024-10-13 18:36:45 -0230

    modify importmap to turn off pins for better_together js

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,10 @@
 pin_all_from File.expand_path('../app/javascript/controllers', __dir__), under: 'controllers'
 
 # Pin the gem's application.js
-pin 'better_together/application', to: 'better_together/application.js'
+# pin 'better_together/application', to: 'better_together/application.js'
 
 # Pin all Stimulus controllers from the gem's app/javascript/better_together/controllers directory
-pin_all_from 'better_together/controllers', under: 'better_together/controllers'
+# pin_all_from 'better_together/controllers', under: 'controllers/better_together'
+
+# Application entry point
+pin 'application', preload: true
