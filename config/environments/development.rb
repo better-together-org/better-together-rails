0d3cb5ca98afb2142ff4b985869da6012b8036c9
Robert Smith <rob@bettertogethersolutions.com>
2024-11-01 14:17:24 -0230

    Configure redis for cache in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,11 @@ Rails.application.configure do # rubocop:todo Metrics/BlockLength
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :redis_cache_store, {
+      url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" },
+      namespace: "cache_wayfinder_development"
+    }
+
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
