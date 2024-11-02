4472e1fcf66140092f0491fc7388aa25ae778f15
Robert Smith <rob@bettertogethersolutions.com>
2024-11-02 07:30:25 -0230

    Update better_together
    Adds partner/community logos and external link click tracking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_31_165500) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_01_152501) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
