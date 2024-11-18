841b37028c5dc43deb49ffea6cff4229ea9e642f
Robert Smith <rob@bettertogethersolutions.com>
2024-11-18 17:51:47 -0330

    Improve partner index performance

--- app/policies/partner_policy.rb
+++ app/policies/partner_policy.rb
@@ -11,7 +11,7 @@ class PartnerPolicy < BetterTogether::CommunityPolicy # rubocop:todo Style/Docum
 
   class Scope < BetterTogether::CommunityPolicy::Scope # rubocop:todo Style/Documentation
     def resolve
-      scope.order(:host, :identifier).includes(:string_translations).with_attached_profile_image.where(permitted_query)
+      scope.order(:identifier).includes(:string_translations).with_attached_profile_image.where(permitted_query)
     end
 
     protected
