fb536816d2b017998b890720b5272eb3245ce998
Robert Smith <rob@bettertogethersolutions.com>
2024-12-06 19:53:15 -0330

    check if exists

--- db/migrate/20241003001443_change_better_together_categories_identifier_unique_index.rb
+++ db/migrate/20241003001443_change_better_together_categories_identifier_unique_index.rb
@@ -4,7 +4,7 @@
 class ChangeBetterTogetherCategoriesIdentifierUniqueIndex < ActiveRecord::Migration[7.1]
   # rubocop:enable Style/Documentation
   def change
-    remove_index :better_together_categories, :identifier
+    remove_index :better_together_categories, :identifier, if_exists: true
     add_index :better_together_categories, %i[identifier type], unique: true
   end
 end
