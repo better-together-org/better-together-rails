d1cebd92ac0c9320046966dde58f4b92958fc734
Robert Smith <rob@bettertogethersolutions.com>
2024-12-06 19:52:22 -0330

    Add migration to generate community records for all existing person records that don't already have a community

--- db/migrate/20240523234216_create_primary_communities_for_existing_people.better_together.rb
+++ db/migrate/20240523234216_create_primary_communities_for_existing_people.better_together.rb
@@ -3,7 +3,9 @@
 # This migration comes from better_together (originally 20240522203205)
 class CreatePrimaryCommunitiesForExistingPeople < ActiveRecord::Migration[7.0]
   def up
-    BetterTogether::Person.where(community_id: nil).each(&:save!) if BetterTogether::Person.where(community_id: nil).any?
+    return unless BetterTogether::Person.where(community_id: nil).any?
+
+    BetterTogether::Person.where(community_id: nil).each(&:save!)
   end
 
   def down; end
