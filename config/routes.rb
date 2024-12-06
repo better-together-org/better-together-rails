63b374a718030c3b3387f673eda22a36810b5c96
Robert Smith <rob@bettertogethersolutions.com>
2024-12-05 21:56:07 -0330

    Remove extra end from locale scope block

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   get 'healthcheck', to: 'healthcheck#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  scope ':locale', # rubocop:todo Metrics/BlockLength
-        locale: /#{I18n.available_locales.join('|')}/ do
-
-    end
+  scope ':locale', locale: /#{I18n.available_locales.join('|')}/ do
   end
 
   root to: redirect("/#{I18n.default_locale}")
