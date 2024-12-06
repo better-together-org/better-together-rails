169fd3953080a09e62038f7cd84d68c1ffa19879
Robert Smith <rob@bettertogethersolutions.com>
2024-12-06 19:49:03 -0330

    Replace deprecated column helper method to set columns for association to community record

--- db/migrate/20240523234216_create_primary_communities_for_existing_people.better_together.rb
+++ db/migrate/20240523234216_create_primary_communities_for_existing_people.better_together.rb
@@ -3,7 +3,7 @@
 # This migration comes from better_together (originally 20240522203205)
 class CreatePrimaryCommunitiesForExistingPeople < ActiveRecord::Migration[7.0]
   def up
-    BetterTogether::Person.where(community_id: nil).each(&:save!)
+    BetterTogether::Person.where(community_id: nil).each(&:save!) if BetterTogether::Person.where(community_id: nil).any?
   end
 
   def down; end
