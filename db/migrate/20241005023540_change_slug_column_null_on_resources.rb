fa0a59aba4da4ffc5162c0db3bab9975f230cb75
Robert Smith <rob@bettertogethersolutions.com>
2024-12-06 23:58:09 -0330

    Remove migrations and other files associated with new to nl project models

--- db/migrate/20241005023540_change_slug_column_null_on_resources.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeSlugColumnNullOnResources < ActiveRecord::Migration[7.1] # rubocop:todo Style/Documentation
-  def change
-    remove_column :better_together_resources, :slug
-  end
-end
