86164316736ccdcc60fdd95de6fa9f7dcb434e24
Robert Smith <rob@bettertogethersolutions.com>
2025-04-09 16:10:07 -0230

    Configure Active Storage to use proxy by default

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -3,4 +3,5 @@
 # config/initializers/active_storage.rb
 Rails.application.config.after_initialize do
   ActiveStorage::Engine.config.active_storage.content_types_to_serve_as_binary.delete 'image/svg+xml'
+  ActiveStorage::Engine.config.active_storage.resolve_model_to_route = :rails_storage_proxy
 end
