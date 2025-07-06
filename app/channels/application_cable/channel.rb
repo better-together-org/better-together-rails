6bd106acd324c196fd754cf914d3849a3ca7c194
Robert Smith <rob@bettertogethersolutions.com>
2025-07-06 15:16:05 -0230

    Update to use aopplciation cable models from betterTogether

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationCable
+  # update to use application channel from BetterTogether
   class Channel < ::BetterTogether::ApplicationChannel
   end
 end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationCable
+  # update to use application connection from BetterTogether
   class Connection < ::BetterTogether::ApplicationConnection
   end
 end
