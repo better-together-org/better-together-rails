afb8ed0246b0d9d791fa277bd3c56722e749f359
Robert Smith <rob@bettertogethersolutions.com>
2025-12-19 13:42:11 -0330

    Rubocop fixes

--- db/migrate/20251218175400_allow_invitee_null_on_better_together_invitations.rb
+++ db/migrate/20251218175400_allow_invitee_null_on_better_together_invitations.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# This migration allows the invitee_type and invitee_id columns
+# in the better_together_invitations table to be null.
+# This change is necessary to accommodate invitations that invite people by email.
 class AllowInviteeNullOnBetterTogetherInvitations < ActiveRecord::Migration[8.0]
   def change
     change_column_null :better_together_invitations, :invitee_type, true
