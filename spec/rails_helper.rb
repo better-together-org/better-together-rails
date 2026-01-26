62d5c16ad9f46667f62686b6144a8115acabe090
Robert Smith <rob@bettertogethersolutions.com>
2026-01-26 13:23:16 -0330

    Rubocop fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,7 @@ Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
+  puts e.to_s.strip # rubocop:disable RSpec/Output
   exit 1
 end
 
