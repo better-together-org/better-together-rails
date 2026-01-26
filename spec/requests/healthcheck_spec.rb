d9d2f3cc4df6e98ae97ed50807383439fb82c203
Robert Smith <rob@bettertogethersolutions.com>
2026-01-26 13:17:03 -0330

    Remove debug output from healthcheck spec

--- spec/requests/healthcheck_spec.rb
+++ spec/requests/healthcheck_spec.rb
@@ -14,11 +14,7 @@ RSpec.describe 'Healthcheck', type: :request do
     it 'returns ok JSON and 200' do # rubocop:todo RSpec/ExampleLength, RSpec/MultipleExpectations
       # rubocop:enable RSpec/MultipleExpectations
       get '/healthcheck'
-      unless response.ok?
-        puts "[DEBUG] /healthcheck status: #{response.status}"
-        puts "[DEBUG] /healthcheck location: #{response.headers['Location']}" if response.redirect?
-        puts "[DEBUG] /healthcheck body: #{response.body.to_s[0, 500]}"
-      end
+
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json).to include('status' => 'ok')
